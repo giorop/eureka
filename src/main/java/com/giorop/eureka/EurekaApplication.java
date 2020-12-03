96370888b436dee43437e9bfc18844000a51a38e
giorop <xiayuren1993@gmail.com>
2020-12-03 12:43:28 +0800

    Initial commit

---
+++ src/main/java/com/giorop/eureka/EurekaApplication.java
@@ -0,0 +1,13 @@
+package com.giorop.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/giorop/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giorop.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
